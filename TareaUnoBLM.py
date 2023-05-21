309c0093cfc79a1906e6b1ce91a3bd98d77e8d50
Bryan Leiva Mejia <129520882+Bryan3214-aiz@users.noreply.github.com>
2023-05-21 00:30:30 -0600

    Update TareaUnoBLM.py

--- TareaUnoBLM.py
+++ TareaUnoBLM.py
@@ -3,18 +3,28 @@
 arrayOne = []
 arrayTwo = []
 
-print("\n---Lista numero 1---\n")
-for i in range(5):
-    numbers = int(input("Ingrese un numero: "))
-    arrayOne.append(numbers)
-    print(arrayOne)
-print("\n---Lista numero 2---\n")
-for x in range(5):
-    numbers = int(input("Ingrese un numero: "))
-    arrayTwo.append(numbers)
-    print(arrayTwo)
+print("\n  Lista numero 1  \n")
+for i in range(5):#Solicita 5 numeros que se almacenaran en "arrayOne"
+    try:
+        numbers = int(input("Ingrese un numero: "))
+        arrayOne.append(numbers)
+    except:#En caso de que se ingrese un valor incorrecto se muestra un mensaje de error y se ejecuta la excepción
+        print("\nIngresaste un valor incorrecto, intentalo de nuevo.\n")
+        numbers = int(input("Ingrese un numero: "))
+        arrayOne.append(numbers)
+
+print("\n  Lista numero 2  \n")
+for i in range(5):#Solicita 5 numeros que se almacenaran en "arrayOne"
+    try:
+        numbers = int(input("Ingrese un numero: "))
+        arrayTwo.append(numbers)
+    except:#En caso de que se ingrese un valor incorrecto se muestra un mensaje de error y se ejecuta la excepción
+        print("\nIngresaste un valor incorrecto, intentalo de nuevo.\n")
+        numbers = int(input("Ingrese un numero: "))
+        arrayTwo.append(numbers)
 
 arrayThree = arrayOne + arrayTwo
 arrayThree.sort(reverse=True)
 
-print(f"\nLista ordenada descendete: {arrayThree}")
+print("\n  LISTAS DE NUMEROS")
+print(f"\nArreglo 1 = {arrayOne}\nArreglo 2 = {arrayTwo}\nArreglo 3 = {arrayThree}")
